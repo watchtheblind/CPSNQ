2d1e94d360c019be47f4fdb43bbae24ea8cd2ea5
Luis Monasterios <ljmm2k2@gmail.com>
2025-01-06 23:24:16 -0400

    refactor: update 'cn' import

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,8 +1,8 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import cn from "./ui/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export function LoginForm({
   className,
@@ -100,5 +100,5 @@ export function LoginForm({
         and <a href="#">Privacy Policy</a>.
       </div>
     </div>
-  )
+  );
 }
